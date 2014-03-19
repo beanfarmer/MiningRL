57e6570f10827dad0a5f443612bf730f772b5821
Goggles <yarrowpb@gmail.com>
2014-03-19 12:21:34 +0000

    Starting cave generation. Needs serious refining.

--- game.js
+++ game.js
@@ -2,9 +2,22 @@ var Game = {
 	display: null,
 	
 	init: function() {
-		this.display = new ROT.Display();
+		this.display = new ROT.Display({width: 80, height: 25});
 		document.body.appendChild(this.display.getContainer());
 		
+		this._generateCaves();	
+	},
+
+	_generateCaves: function() {
+		var w = 80;
+		var h = 25;
+		var map = new ROT.Map.Cellular(w, h);
+		
+		map.randomize(0.5);
+
+		for (var i=0; i < 4; i++) {
+			map.create(this.display.DEBUG);
+		}
 	}
 
 };
