4a9e3c8987bcccf69ce91d79d236054ae5b465f9
Goggles <yarrowpb@gmail.com>
2014-03-19 16:59:28 +0000

    Caves are now generated properly and displayed in ascii. # for walls and . for traversible floor.

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 var Game = {
 	display: null,
-	
+	map: {},	
+
+
 	init: function() {
 		this.display = new ROT.Display({width: 80, height: 40, fontSize: 14});
 		document.body.appendChild(this.display.getContainer());
@@ -16,11 +18,36 @@ var Game = {
 			survive: [2, 3, 4, 5]});
 		
 		map.randomize(0.8);
+	
+		var cellCallback = function(x, y, value) {
+			var key;
+
+			if (value) {	/* eg, if this point is a wall. */
+				key = x+","+y;
+				this.map[key] = "#";
+				return; /* Don't need to carry on and accidentally write the wrong character */
+			}
+			key = x+","+y;
+			this.map[key] = ".";
+		}
 
 		for (var i=49; i >=0; i--) {
-			map.create(i ? null : this.display.DEBUG);
+			map.create(i ? null : cellCallback.bind(this));
 		}
+		
+		this._drawWholeMap();
+	},
+
+	_drawWholeMap: function() {
+		for (var key in this.map) {
+			var parts = key.split(",");
+			var x = parseInt(parts[0]);
+			var y = parseInt(parts[1]);
+			this.display.draw(x, y, this.map[key]);
+		}
+
 	}
+	
 
 };
 
